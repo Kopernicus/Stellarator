361ba00215bff276844e0768d8a9ed3024ca51d6
Martin Dünkelmann <nc-duenkekl3@netcologne.de>
2017-08-06 22:52:04 +0200

    Optimized Code (C#7)
    Added ToDo's

--- Source/Program.cs
+++ Source/Program.cs
@@ -11,33 +11,35 @@ using System.Threading;
 
 namespace Stellarator
 {
-    public class Program
+    public static class Program
     {
         /// <summary>
-        /// The entrypoint for our application.
+        ///     The entrypoint for our application.
         /// </summary>
         /// <param name="args"></param>
-        public static Int32 Main(string[] args)
+        public static int Main(string[] args)
         {
             // Say hello
             Console.WriteLine("-------------------------------------------------------------");
             Console.WriteLine("Stellar Generator - Creates procedural systems for Kopernicus");
             Console.WriteLine("Copyright (c) 2016 Thomas P.");
             Console.WriteLine("Licensed under the Terms of the MIT License");
-            Console.WriteLine("Version: 0.1");
+            Console.WriteLine("Version: 0.2");
             Console.WriteLine("-------------------------------------------------------------");
 
             // Can I have some tea?
             Thread.CurrentThread.CurrentCulture = CultureInfo.GetCultureInfo("en-US");
 
+            //TODO: MartinX3 Change How we get the args. Only show question, if arg is not given.
             // Ask for Input
-            String seed = Prompt("Please enter the Seed you want to use: ", "--seed");
-            String folder = Prompt("Please choose a folder name for your system: ", "--name");
-            String systematic = Prompt("Use systematic planet names? (y/n) ", "--systematic", true);
+            var seed = Prompt("Please enter the seed you want to use: ", "--seed");
+            var folder = Prompt("Please choose a folder name for your system: ", "--name");
+            var systematic = Prompt("Use systematic planet names? (y/n) ", "--systematic", true);
             Console.WriteLine();
 
+            //TODO: Is there a better way? y/n
             // Check
-            if (systematic != "y" && systematic != "n")
+            if ((systematic != "y") && (systematic != "n"))
             {
                 Console.WriteLine("Invalid Input! Enter either y or n.");
                 return -1;
@@ -52,14 +54,15 @@ namespace Stellarator
         }
 
         /// <summary>
-        /// Asks the user something and returns an answer.
+        ///     Asks the user something and returns an answer.
         /// </summary>
-        public static String Prompt(String prompt, String cmdLine, Boolean key = false)
+        private static string Prompt(string prompt, string cmdLine, bool key = false)
         {
-            String[] args = Environment.GetCommandLineArgs();
+            //TODO: MartinX3 Change How we get the args. Only show question, if arg is not given.
+            var args = Environment.GetCommandLineArgs();
             if (args.Any(s => s.Trim().StartsWith(cmdLine)))
             {
-                String arg = args.First(s => s.Trim().StartsWith(cmdLine));
+                var arg = args.First(s => s.Trim().StartsWith(cmdLine));
                 arg = arg.Trim().Remove(0, (cmdLine + ":").Length);
                 Console.WriteLine(prompt + arg);
                 return arg;
